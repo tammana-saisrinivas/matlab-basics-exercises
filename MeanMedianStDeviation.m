25fa7ef39a875be9c6053babeb6326ea3888df60
TAMMANA SAISRINIVAS <saisrinivastammana@gmail.com>
2025-06-26 20:52:29 +0530

    Create MeanMedianStDeviation.m

---
+++ MeanMedianStDeviation.m
@@ -0,0 +1,7 @@
+data = [5, 10, 15, 20, 25];
+
+mean_val = mean(data);
+median_val = median(data);
+std_dev = std(data);
+
+fprintf('Mean: %.2f\nMedian: %.2f\nStd Dev: %.2f\n', mean_val, median_val, std_dev);
